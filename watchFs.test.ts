02f683029f5e9c4b04d5e6c47a62f699a0b10db9
jldec <jurgen@jldec.me>
2024-06-10 22:39:12 +0100

    use normalized paths for memoryFs

--- watchFs.test.ts
+++ watchFs.test.ts
@@ -42,6 +42,21 @@ describe.each(await testEnvironments())(
 	"watchFs $envName",
 	async ({ envName, nodeishFs, baseDir }) => {
 		const waitForWatch = 100
+		const isMemory = envName === "memory"
+
+		// memoryFs watch seems to prefer forward slashes
+		// TODO: normalize paths for memoryFs
+		// and only memoryFs has consistent event counts across OS flavors
+		// => can't easily count watch events for node in these tests
+
+		const filename1 = "message.json"
+		const filepath = isMemory ? baseDir + "/" + filename1 : join(baseDir, filename1)
+		const dirname = "subdir"
+		const dirpath = isMemory ? baseDir + "/" + dirname : join(baseDir, dirname)
+		const filename2 = "foo.bar"
+		const dirfilepath = isMemory ? dirpath + "/" + filename2 : join(dirpath, filename2)
+		const dirfilename = isMemory ? dirname + "/" + filename2 : join(dirname, filename2)
+
 		it(
 			"emits events when files are touched",
 			async () => {
@@ -56,43 +71,40 @@ describe.each(await testEnvironments())(
 				const observable = watchFs({ nodeishFs, baseDir })
 				const subscription = observable.subscribe(observer)
 
-				await nodeishFs.writeFile(join(baseDir, "messages.json"), "{}")
+				await nodeishFs.writeFile(filepath, "{}")
 				await sleep(waitForWatch)
 
-				// only memoryFs has consistent event counts across different OS
-				if (envName === "memory") {
+				if (isMemory) {
 					expect(observer.next).toHaveBeenCalledTimes(1)
 				}
-				expect(observer.next).toHaveBeenCalledWith("messages.json")
+				expect(observer.next).toHaveBeenCalledWith(filename1)
 
-				await nodeishFs.rm(join(baseDir, "messages.json"))
+				await nodeishFs.rm(join(baseDir, filename1))
 				await sleep(waitForWatch)
 
-				if (envName === "memory") {
+				if (isMemory) {
 					expect(observer.next).toHaveBeenCalledTimes(2)
 				}
-				expect(observer.next).toHaveBeenCalledWith("messages.json")
+				expect(observer.next).toHaveBeenCalledWith(filename1)
 
-				await nodeishFs.mkdir(join(baseDir, "subdir"), { recursive: true })
+				await nodeishFs.mkdir(dirpath, { recursive: true })
 				await sleep(waitForWatch)
 
-				if (envName === "memory") {
+				if (isMemory) {
 					expect(observer.next).toHaveBeenCalledTimes(3)
 				}
-				expect(observer.next).toHaveBeenCalledWith("subdir")
+				expect(observer.next).toHaveBeenCalledWith(dirname)
 
-				await nodeishFs.writeFile(join(baseDir, "subdir", "foo.bar"), "{}")
+				await nodeishFs.writeFile(dirfilepath, "{}")
 				await sleep(waitForWatch)
 
-				if (envName === "memory") {
+				if (isMemory) {
 					expect(observer.next).toHaveBeenCalledTimes(4)
 				}
-
 				// node versions <20 do not support recursive watch
 				// https://github.com/nodejs/node/pull/45098#issuecomment-1891612491
-				if (envName === "memory" || parseInt(process.version.slice(1, 3)) >= 20) {
-					// memoryFs watch always reports with forward slashes
-					expect(observer.next).toHaveBeenCalledWith(envName === "memory" ? "subdir/foo.bar" : join("subdir", "foo.bar"))
+				if (isMemory || parseInt(process.version.slice(1, 3)) >= 20) {
+					expect(observer.next).toHaveBeenCalledWith(dirfilename)
 				}
 				const nextCallCount = observer.next.mock.calls.length
 
@@ -105,7 +117,7 @@ describe.each(await testEnvironments())(
 				expect(observer.complete).toHaveBeenCalledTimes(1)
 
 				// should not emit any more events
-				await nodeishFs.writeFile(join(baseDir, "messages.json"), "{}")
+				await nodeishFs.writeFile(join(baseDir, filename1), "{}")
 				await sleep(waitForWatch)
 
 				expect(observer.next).toHaveBeenCalledTimes(nextCallCount)
